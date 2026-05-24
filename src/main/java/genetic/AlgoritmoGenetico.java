8902175b450d8551c92d73dae4e8d6eca1ad0e0e
Xen <aron.laika@gmail.com>
2026-05-24 16:11:47 +0200

    feat: estructura del algoritmo genetico

---
+++ src/main/java/genetic/AlgoritmoGenetico.java
@@ -0,0 +1,16 @@
+package genetic;
+
+public class AlgoritmoGenetico {
+    /*
+     * Necesitamos seguir unos pasos:
+     * 1. Poblacion inicial -> Generar una poblacion de permutaciones aleatorias. Por ejemplo, 100 permutaciones
+     * 2. Evaluamos esas permutaciones. Podemos utilizar split + combinacion de los operadores que ya tengo. Podemos crear una nueva clase que sea un individuo
+     * 3. Entramos en un bucle que tendremos que gestionar si no encontramos mejora en un numero de iteraciones determinadas
+     * 4. Seleccionamos pares de soluciones aleatorias
+     * 5. Hacemos cruces entre cada par
+     * 6. Hacemos mutaciones
+     * 7. Evaluamos las nuevas permutaciones. Con split y busqueda local
+     * 8. Reemplazamos mediante torneo 2/4. Por ejemplo, cogemos dos padres y dos hijos y nos quedamos con los mejores
+     * 9. Volvemos a ejecutar el bucle del punto 3
+     */
+}
